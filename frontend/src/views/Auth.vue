aec3a63563cce64e751112a61adb4b2f091fd3a6
vunqf8 <vunqf8@fullstack.edu.vn>
2026-04-10 20:45:15 +0700

    Final

--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -63,15 +63,10 @@ const submit = async () => {
       router.push("/home");
     } else {
       isLogin.value = true;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       error.value = "";
-=======
-      error.value = ""; // clear
->>>>>>> e1996a4 (Feat: Update UI Login/Register)
-=======
+
       error.value = "";
->>>>>>> bee8aaf (Fix: UI)
     }
   } catch (e) {
     error.value = isLogin.value
@@ -105,15 +100,6 @@ const submit = async () => {
           <span>{{ error }}</span>
         </div>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <!-- FIRST NAME + LAST NAME -->
-=======
-        <!-- FIRST NAME + LAST NAME — chỉ hiện khi đăng ký -->
->>>>>>> e1996a4 (Feat: Update UI Login/Register)
-=======
-        <!-- FIRST NAME + LAST NAME -->
->>>>>>> bee8aaf (Fix: UI)
         <transition name="slide">
           <div v-if="!isLogin" class="flex gap-2">
             <div class="flex-1">
@@ -215,14 +201,7 @@ const submit = async () => {
   overflow: hidden;
   max-height: 100px;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> e1996a4 (Feat: Update UI Login/Register)
-=======
-
->>>>>>> bee8aaf (Fix: UI)
 .slide-enter-from,
 .slide-leave-to {
   max-height: 0;
